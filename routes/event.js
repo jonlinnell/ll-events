6d47a5e6cc4b2ce28cd19f8228a068f73e190c40
Jon Linnell <linnell.jon@gmail.com>
2017-11-12 14:54:17 +0000

    Additional event routes (outline)

--- routes/event.js
+++ routes/event.js
@@ -2,6 +2,7 @@ const Joi = require('joi')
 
 const models = require('../models')
 const eventCreateSchema = require('../schemas/eventCreate')
+const eventUpdateSchema = require('../schemas/eventUpdate')
 
 module.exports = (app) => {
   app.get('/', (req, res) => {
@@ -19,6 +20,47 @@ module.exports = (app) => {
         models.Event.create(req.body)
           .then((event) => {
             res.json(event)
+          })
+      }
+    })
+  })
+
+  app.get('/:id', (req, res) => {
+    models.Event.findById(req.params.id)
+      .then((event) => {
+        res.json(event)
+      })
+  })
+
+  app.put('/:id', (req, res) => {
+    Joi.validate(req.body, eventUpdateSchema, (error) => {
+      if (error !== null) {
+        res.sendStatus(400)
+      } else {
+        models.Event.update(req.body, {
+          where: {
+            id: req.params.id
+          }
+        })
+          .then(() => {
+            models.Event.findById(req.params.id)
+              .then((event) => {
+                res.json(event)
+              })
+          })
+      }
+    })
+  })
+
+  app.delete('/:id', (req, res) => {
+    Joi.validate(req.body, eventUpdateSchema, (error) => {
+      if (error !== null) {
+        res.sendStatus(400)
+      } else {
+        models.Event.findById(req.params.id)
+          .then((event) => {
+            event.destroy()
+            res.sendStatus(200)
           })
       }
     })
